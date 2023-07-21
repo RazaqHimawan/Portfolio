4810046b4e5566b501e3d25545c2cf66015f987c
RazaqHimawan <razaqhim@gmail.com>
2023-07-21 12:44:22 +0700

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,38 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+import { escapeSvelte, mdsvex } from 'mdsvex';
+import shiki from 'shiki';
+import remarkUnwrapImages from 'remark-unwrap-images';
+import remarkToc from 'remark-toc';
+import rehypeSlug from 'rehype-slug';
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexConfig = {
+  extensions: ['.md'],
+  layout: {
+    _: '/src/mdsvex.svelte',
+  },
+  highlight: {
+    highlighter: async (code, lang = 'text') => {
+      const highlighter = await shiki.getHighlighter({
+        theme: 'dark-plus',
+      });
+      const html = escapeSvelte(highlighter.codeToHtml(code, { lang }));
+      return `{@html \`${html}\`}`;
+    },
+  },
+  remarkPlugins: [remarkUnwrapImages, remarkToc],
+  rehypePlugins: [rehypeSlug],
+};
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
+  extensions: ['.svelte', '.md'],
+  kit: {
+    adapter: adapter(),
+  },
+};
+
+export default config;
