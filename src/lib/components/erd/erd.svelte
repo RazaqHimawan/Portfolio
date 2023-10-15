f7eca1940ecd9a1f4996a4be7a71aaca27ef9f7a
RazaqHimawan <razaqhim@gmail.com>
2023-10-15 13:35:49 +0700

    Add mermaid support

---
+++ src/lib/components/erd/erd.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import mermaid from 'mermaid';
+import { onMount } from 'svelte';
+
+let erd: HTMLPreElement;
+
+mermaid.initialize({
+  startOnLoad: false,
+  theme: 'neutral',
+});
+
+onMount(() => {
+  mermaid.run({ nodes: [erd] });
+});
+</script>
+
+<pre
+  class="mermaid"
+  bind:this={erd}>
+    <slot />
+</pre>
